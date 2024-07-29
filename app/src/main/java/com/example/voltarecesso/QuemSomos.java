ddca688cd0d686c6ca298c1f01174ba0acc60695
edcarlos <edcarlos281282@gmail.com>
2024-07-29 12:02:15 -0300

    Volta do Recesso

---
+++ app/src/main/java/com/example/voltarecesso/QuemSomos.java
@@ -0,0 +1,14 @@
+package com.example.voltarecesso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuemSomos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quem_somos);
+    }
+}
